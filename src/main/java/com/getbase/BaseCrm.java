3c9ce5244515de3c51c55d68f65c19ce42ba5f8e
iaintshine <bodziomista@gmail.com>
2015-04-30 12:19:14 +0200

    put dots on the left side of expressions instead of end of line

--- src/main/java/com/getbase/BaseCrm.java
+++ src/main/java/com/getbase/BaseCrm.java
@@ -7,17 +7,17 @@ import com.getbase.services.LeadsService;
 
 public class BaseCrm {
     public static void main(String[] args) {
-        Client client = new Client(new Configuration.Builder().
-                accessToken(System.getenv("BASECRM_ACCESS_TOKEN")).
-                verbose().
-                userAgent(Configuration.DEFAULT_USER_AGENT + "+tests").
-                build());
+        Client client = new Client(new Configuration.Builder()
+                .accessToken(System.getenv("BASECRM_ACCESS_TOKEN"))
+                .verbose()
+                .userAgent(Configuration.DEFAULT_USER_AGENT + "+tests")
+                .build());
 
-        client.leads().list(new LeadsService.QueryParamBuilder().
-                page(1).
-                perPage(10)).
-                stream().
-                forEach(System.out::println);
+        client.leads().list(new LeadsService.QueryParamBuilder()
+                .page(1)
+                .perPage(10))
+                .stream()
+                .forEach(System.out::println);
 
         Lead mark = new Lead();
         mark.setFirstName("Mark");

6e0cb292eb09cb4bb051f4f15dd90e6ce8ce9b28
Olaf Sniezek <olafsniezek@gmail.com>
2018-12-07 17:46:24 +0100

    Added missing visited_at field to Visit

--- src/main/java/com/getbase/models/Visit.java
+++ src/main/java/com/getbase/models/Visit.java
@@ -6,14 +6,13 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.getbase.serializer.Views;
 import org.joda.time.DateTime;
 
-
-
 public class Visit {
   protected @JsonView(Views.ReadOnly.class) Long id;
   protected @JsonView(Views.ReadOnly.class) Long creatorId;
   protected @JsonView(Views.ReadOnly.class) Long outcomeId;
   protected @JsonView(Views.ReadOnly.class) Long resourceId;
   protected @JsonView(Views.ReadOnly.class) DateTime createdAt;
+  protected @JsonView(Views.ReadOnly.class) DateTime visitedAt;
   protected @JsonView(Views.ReadOnly.class) String repLocationVerificationStatus;
   protected @JsonView(Views.ReadOnly.class) String resourceAddress;
   protected @JsonView(Views.ReadOnly.class) String resourceType;
@@ -43,6 +42,10 @@ public class Visit {
     return this.createdAt;
   }
 
+  public DateTime getVisitedAt() {
+    return this.createdAt;
+  }
+
   public String getRepLocationVerificationStatus() {
     return this.repLocationVerificationStatus;
   }
@@ -66,17 +69,18 @@ public class Visit {
   @Override
   public String toString() {
     return "Visit{" +
-          "id=" + id +
-          ", creatorId=" + creatorId +
-          ", outcomeId=" + outcomeId +
-          ", resourceId=" + resourceId +
-          ", createdAt=" + createdAt +
-          ", repLocationVerificationStatus='" + repLocationVerificationStatus + '\'' +
-          ", resourceAddress='" + resourceAddress + '\'' +
-          ", resourceType='" + resourceType + '\'' +
-          ", summary='" + summary + '\'' +
-          ", updatedAt=" + updatedAt +
-          "}";
+            "id=" + id +
+            ", creatorId=" + creatorId +
+            ", outcomeId=" + outcomeId +
+            ", resourceId=" + resourceId +
+            ", createdAt=" + createdAt +
+            ", visitedAt=" + visitedAt +
+            ", repLocationVerificationStatus='" + repLocationVerificationStatus + '\'' +
+            ", resourceAddress='" + resourceAddress + '\'' +
+            ", resourceType='" + resourceType + '\'' +
+            ", summary='" + summary + '\'' +
+            ", updatedAt=" + updatedAt +
+            "}";
   }
 
   @Override
@@ -91,6 +95,7 @@ public class Visit {
     if (outcomeId != null ? !outcomeId.equals(visit.outcomeId) : visit.outcomeId != null) return false;
     if (resourceId != null ? !resourceId.equals(visit.resourceId) : visit.resourceId != null) return false;
     if (createdAt != null ? !createdAt.equals(visit.createdAt) : visit.createdAt != null) return false;
+    if (visitedAt != null ? !visitedAt.equals(visit.visitedAt) : visit.visitedAt != null) return false;
     if (repLocationVerificationStatus != null ? !repLocationVerificationStatus.equals(visit.repLocationVerificationStatus) : visit.repLocationVerificationStatus != null) return false;
     if (resourceAddress != null ? !resourceAddress.equals(visit.resourceAddress) : visit.resourceAddress != null) return false;
     if (resourceType != null ? !resourceType.equals(visit.resourceType) : visit.resourceType != null) return false;
@@ -108,6 +113,7 @@ public class Visit {
     result = 31 * result + (outcomeId != null ? outcomeId.hashCode() : 0);
     result = 31 * result + (resourceId != null ? resourceId.hashCode() : 0);
     result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+    result = 31 * result + (visitedAt != null ? visitedAt.hashCode() : 0);
     result = 31 * result + (repLocationVerificationStatus != null ? repLocationVerificationStatus.hashCode() : 0);
     result = 31 * result + (resourceAddress != null ? resourceAddress.hashCode() : 0);
     result = 31 * result + (resourceType != null ? resourceType.hashCode() : 0);
